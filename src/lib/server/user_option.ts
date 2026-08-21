24075784dd52b89b403a6d6aa3e7140c75f2ed3e
Moritz Löwenstein <moritz.loewenstein@gmx.de>
2026-08-22 01:12:28 +0200

    format

--- src/lib/server/user_option.ts
+++ src/lib/server/user_option.ts
@@ -2,46 +2,46 @@ import { DbNull, type JsonValue } from '@prisma/client/runtime/client';
 import { prisma } from './db';
 
 class UserOptionClass {
-    async set(userId: string, key: string, value: JsonValue): Promise<void> {
-        await prisma.userOption.upsert({
-            create: {
-                userId,
-                key,
-                value: value === null ? DbNull : value
-            },
-            update: {
-                value: value === null ? DbNull : value
-            },
-            where: {
-                userId_key: {
-                    userId,
-                    key
-                }
-            }
-        });
-    }
+	async set(userId: string, key: string, value: JsonValue): Promise<void> {
+		await prisma.userOption.upsert({
+			create: {
+				userId,
+				key,
+				value: value === null ? DbNull : value
+			},
+			update: {
+				value: value === null ? DbNull : value
+			},
+			where: {
+				userId_key: {
+					userId,
+					key
+				}
+			}
+		});
+	}
 
-    async get(userId: string, key: string): Promise<JsonValue> {
-        const row = await prisma.userOption.findUnique({
-            where: {
-                userId_key: {
-                    userId,
-                    key
-                }
-            }
-        });
-        return row?.value || null;
-    }
+	async get(userId: string, key: string): Promise<JsonValue> {
+		const row = await prisma.userOption.findUnique({
+			where: {
+				userId_key: {
+					userId,
+					key
+				}
+			}
+		});
+		return row?.value || null;
+	}
 
-    async delete(userId: string, key: string): Promise<void> {
-        // deleteMany: do not error if option does not exist
-        await prisma.userOption.deleteMany({
-            where: {
-                userId,
-                key
-            }
-        });
-    }
+	async delete(userId: string, key: string): Promise<void> {
+		// deleteMany: do not error if option does not exist
+		await prisma.userOption.deleteMany({
+			where: {
+				userId,
+				key
+			}
+		});
+	}
 }
 
 const instance = new UserOptionClass();
