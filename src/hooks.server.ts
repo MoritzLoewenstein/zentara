d1bb791121446584418474396dfcab85d53b2034
Moritz Löwenstein <moritz.loewenstein@gmx.de>
2026-08-29 01:50:41 +0200

    delay webhook registration after sveltekit http server listens

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,17 @@
 import { db } from '$lib/server/db';
 import strava from '$lib/server/strava';
 import polarflow from '$lib/server/polarflow';
-import type { HandleServerError } from '@sveltejs/kit';
+import type { HandleServerError, ServerInit } from '@sveltejs/kit';
 
-db.init();
-await Promise.all([strava.init(), polarflow.init()]);
+export const init: ServerInit = async () => {
+	db.init();
+	// these are async functions, but we do not await them
+	// because we need the http server to start for them to work
+	setTimeout(() => {
+		strava.init().catch((err) => console.error('strava init error:', err));
+		polarflow.init().catch((err) => console.error('polarflow init error:', err));
+	}, 10);
+};
 
 export const handleError: HandleServerError = async ({ error, message }) => {
 	console.error(error);
